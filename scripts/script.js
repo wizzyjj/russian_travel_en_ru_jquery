b02f74c891790e7c31ad67c53a043c4e52b2c9c3
Artur Adamaites <wizzyjjj@gmail.com>
2022-10-30 04:53:18 +0300

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+});
+
+$(".__lang.rus_langb").click(function(){
+    //alert("Нажата кнопка АНГЛИЙСКИЙ");
+    var rusLang = document.getElementsByClassName("rus_lang");
+    var enLang = document.getElementsByClassName("en_lang");
+    for (i = 0; i < enLang.length; i++) {
+      enLang[i].style.position = "static";
+      rusLang[i].style.position = "absolute";
+      rusLang[i].style.top = "-9999px";
+      rusLang[i].style.left = "-9999px";
+    }
+  });
+
+    $(".__lang.en_langb").click(function(){
+      //alert("Нажата кнопка АНГЛИЙСКИЙ");
+      var rusLang = document.getElementsByClassName("rus_lang");
+      var enLang = document.getElementsByClassName("en_lang");
+      for (i = 0; i < enLang.length; i++) {
+        rusLang[i].style.position = "static";
+        enLang[i].style.position = "absolute";
+        enLang[i].style.top = "-9999px";
+        enLang[i].style.left = "-9999px";
+      }
+  });
